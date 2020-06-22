4e256ed2fb3edb0ce32ae5604f697e3c837ae7cd
ljsgeass <ljsgeass@126.com>
2020-06-22 19:57:07 +0800

    Initial commit

---
+++ src/main/java/comatguigu/Test1.java
@@ -0,0 +1,18 @@
+package comatguigu;
+
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: ljsgeass
+ * @Date: 2020/06/22/19:49
+ * @Description:
+ */
+public class Test1{
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+
+    }
+}
